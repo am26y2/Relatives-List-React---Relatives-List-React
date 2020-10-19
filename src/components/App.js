f9fb5ad80b903903c3193478444d681aea7c09ce
Amit Raj <amitraj9046@gmail.com>
2020-10-19 14:04:44 +0530

    Relatives List React

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  relatives = ["Amit", "Alok", "Gaurav", "Ankit"];
+  render() {
+    return (
+      <ol key="relativeList">
+        {this.relatives.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>{relative}</li>
+        ))}
+      </ol>
+    );
+  }
 }
-
 
 export default App;
