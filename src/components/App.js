9c9233f5501d80a21720f50b669d0a74985c129b
Amit Raj <amitraj9046@gmail.com>
2020-10-19 14:26:47 +0530

    Relatives List React

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <ol key="relativeList">
-        {this.relatives.map((relative, index) => (
+        {relatives.map((relative, index) => (
           <li key={`relativeListItem${index + 1}`}>{relative}</li>
         ))}
       </ol>
